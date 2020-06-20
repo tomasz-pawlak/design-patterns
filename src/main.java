2692b4ffc97bff2c9580de86e74eda5e4142780f
tom <gurtoc@gmail.com>
2020-06-20 23:00:38 +0200

    builder

--- src/main.java
+++ src/main.java
@@ -1,20 +1,5 @@
 public class main {
     public static void main(String[] args) {
-        GuessGame guessGame = GuessGame.getInstance();
-
-        guessGame.play();
-
-        int score = guessGame.getScore();
-
-        GuessGame anotherGameReference = GuessGame.getInstance();
-
-        if (guessGame == anotherGameReference) {
-            System.out.println("Singleton!");
-            if (score == anotherGameReference.getScore()) {
-                System.out.println("I punkty się zgadzają!");
-            }
-        }
-        System.out.println("Liczba punktów to:" + score);
 
     }
 
