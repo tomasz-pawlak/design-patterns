13484fa5c7654e922a34bcde1e5f4db8ee43dc71
tom <gurtoc@gmail.com>
2020-06-24 20:26:08 +0200

    practice

--- src/main.java
+++ src/main.java
@@ -1,31 +1,12 @@
-package House;
+import Flight.FlightLeg;
 
 public class main {
 
-    //Z klasą wewnętrzną - Builder używany, gdy ma bardzo dużo argumentów(pól), nie chcemy dużej liczby constr,
-    //użytkownik bez dostepu do setterów, ustawianie wartości przy tworzeniu, ale po stworzeniu bez mozliwosci zmian
-
-    //Z interface(klasyczny) - bez mozliwosci ustawiania wartosci na starcie, dyrektor zajmuje sie wszystkim.
-
-    //poznac mozna po strukturze
-
-    //Można używać Lombok, który przyśpieszy tworzenie @Builder
     public static void main(String[] args) {
 
-        SmallHouse smallHouseBuilder = new SmallHouse();
-        BigHouse bigHouseBuilder = new BigHouse();
+        FlightLeg leg = new FlightLeg.FlightLegBuilder("Las Vegas", "Los Angeles").price(50).build();
 
-        HouseDirector smallhouseDirector = new HouseDirector(smallHouseBuilder);
-        HouseDirector bigHouseDirector = new HouseDirector(bigHouseBuilder);
-
-        smallhouseDirector.buildHouse();
-        bigHouseDirector.buildHouse();
-
-        House smallHouse = smallhouseDirector.getHouse();
-        House bigHouse = bigHouseDirector.getHouse();
-
-        System.out.println(smallHouse);
-        System.out.println(bigHouse);
+        System.out.println(leg);
 
 
     }
